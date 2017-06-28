2cbd1f3ff89ad7eb0bfe6028d2f4311c8dc1f0cf
George Biryukov <george@biryukov.ws>
2017-06-28 22:34:51 +0300

    add tests

---
+++ test/env.spec.js
@@ -0,0 +1,12 @@
+const expect = require('expect')
+const Env = require('../src/env.js');
+
+describe('Env spec', () => {
+	it('Should have public methods', () => {
+		expect(Env.set).toBeA('function');
+		expect(Env.get).toBeA('function');
+		expect(Env.ready).toBeA('function');
+		expect(Env.onReady).toBeA('function');
+		expect(Env.clear).toBeA('function');
+	});
+});
---
+++ test/env.test.js
@@ -0,0 +1,45 @@
+const expect = require('expect')
+const Env = require('../src/env.js');
+
+describe('Env', () => {
+	beforeEach(() => {
+		Env.clear();
+	})
+
+	it('Should assign variable by key into Env via `set` method', () => {
+		Env.set('foo', 1);
+		expect(Env.get('foo')).toEqual(1);
+	});
+
+	it('Should assign all keys into Env via `set` method when object passed', () => {
+		Env.set({
+			foo: 1,
+			bar: 'baz',
+		});
+
+		expect(Env.get('foo')).toEqual(1);
+		expect(Env.get('bar')).toEqual('baz');
+	});
+
+	it('Should return `false` for not existing keys', () => {
+		expect(Env.get('foo')).toEqual(false);
+	});
+
+	it('Should remove all vars by calling `clear` method', () => {
+		Env.set('foo', 1);
+		Env.clear();
+		expect(Env.get('foo')).toEqual(false);
+	});
+
+	it('Should call all callbacks after `ready` invoke', () => {
+		Env.set('foo', 1);
+		Env.set('bar', 2);
+		Env.onReady(() => {
+			expect(Env.get('foo')).toEqual(1);
+		});
+		Env.onReady(() => {
+			expect(Env.get('bar')).toEqual(2);
+		});
+		Env.ready();
+	});
+});
